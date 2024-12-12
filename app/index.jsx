2827178948cdf4451c217d3a0bc15cc48f0e167f
Albert735 <03jayteee@gmail.com>
2024-12-12 03:07:34 +0000

    `Refactor index.jsx layout and styles`

--- app/index.jsx
+++ app/index.jsx
@@ -10,34 +10,34 @@ const backgroundImage = require("../assets/images/blujacket Background Removed.p
 export default function index() {
   return (
     <SafeAreaView className="flex-1 bg-[#02196F]">
-      <Text
-        style={{ fontFamily: "Modak" }}
-        className="text-[#3666AB] text-[12rem] leading-tight absolute top-20 right-10 Tantor"
-      >
-        Puff
-      </Text>
+      <View className="flex flex-col items-center justify-start ">
+        <Text
+          style={{ fontFamily: "Modak" }}
+          className="text-[#3666AB] text-[12rem] text-center"
+        >
+          Puff
+        </Text>
+        <Text
+          style={{ fontFamily: "Modak" }}
+          className="text-[#9ab8e1] text-[3rem] text-right px-5 absolute bottom-0"
+        >
+          Jacket
+        </Text>
+      </View>
+
       <StatusBar barStyle="light-content" />
       <ImageBackground
         source={backgroundImage}
-        resizeMode="cover"
-        className="flex-1 "
+        resizeMode="contain"
+        className="flex-1"
       >
-        <View className="flex flex-col justify-between w-screen h-full">
-          <View className="flex flex-col items-start justify-start px-4">
-            <Text
-              style={{ fontFamily: "Modak" }}
-              className="flex text-[#b4caea] mix-blend-multiply text-[2.5rem] Tantor text-right absolute top-40 right-5 font-bold"
-            >
-              Jackets
-            </Text>
-          </View>
-
+        <View className="flex flex-col justify-end w-screen h-full ">
           <View className="flex items-start justify-start gap-9 px-5 ">
             <View className="flex flex-col items-start justify-start gap-4">
               <Text className="text-white text-[1.5rem] Tantor font-bold">
                 Elevate your style
               </Text>
-              <Text className="text-white text-[1.2rem] Tantor leading-relaxed font-light">
+              <Text className="text-white text-[1.2rem] Tantor leading-relaxed font-normal text-justify">
                 Discover innovative,padded leather jacketsfor ultimate comfort
                 and style
               </Text>
@@ -45,7 +45,7 @@ export default function index() {
             <View className="w-full">
               <Link href="/home" asChild>
                 <TouchableOpacity className="text-white bg-[#3666AB] rounded-full text-[1.5rem]  Tantor w-full">
-                  <Text className="text-center text-[#ccdcf4] text-[1.5rem] Tantor font-bold py-7">
+                  <Text className="text-center text-[#ffffff] text-[1.5rem] Tantor font-bold py-7">
                     Quick Shop Access
                   </Text>
                 </TouchableOpacity>
