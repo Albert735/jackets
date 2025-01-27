2a7063f3f4cad216a3035f250522206929e42bd5
Albert735 <03jayteee@gmail.com>
2025-01-27 00:32:52 +0000

    `Added AppLoading component for font loading state`

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,18 @@
-import { View, Text } from "react-native";
-import React from "react";
 import "../global.css";
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function _layout() {
   const [loaded] = useFonts({
     Tantor: require("../assets/fonts/Tantor.ttf"),
     Modak: require("../assets/fonts/Modak-Regular.ttf"),
   });
+
   if (!loaded) {
-    return null;
+    return <AppLoading />;
   }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
